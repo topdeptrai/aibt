77098c08a4ffc6aada59525ecf6f912e6f56ffc4
topdeptrai <topdeptrai01@gmail.com>
2024-11-23 08:59:16 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,7 @@
+getgenv().Team = "Marines" -- Marines Pirates
+getgenv().AutoLoad = false --Will Load Script On Server Hop
+getgenv().SlowLoadUi  = false
+getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+getgenv().ForceUseSilentAim = false --Force turn on silent aim , if error then executor problem
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
